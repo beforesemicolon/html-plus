cf384985c6fef54cc510b55e709d8e347f5bd863
Elson Correia <design@elsoncorreia.com>
2021-06-06 15:21:33 -0400

    landing page

--- website/server-test.js
+++ website/server-test.js
@@ -3,17 +3,22 @@ const http = require('http');
 const path = require('path');
 const {engine} = require('../core/engine');
 const pages = require('./pages/data/pages.json');
+const site = require('./pages/data/site.json');
+const packageJSON = require('./../package.json');
 
 const app = express();
 
 engine(app, path.resolve(__dirname, './pages'), {
   staticData: {
-    pages
-  }
+    pages,
+    site,
+    version: packageJSON.version
+  },
+  customTags: []
 });
 
 const server = http.createServer(app);
 
 server.listen(3000, () => {
-  console.log('listening on port 3000');
+  console.log('listening on port http://localhost:3000/');
 })
