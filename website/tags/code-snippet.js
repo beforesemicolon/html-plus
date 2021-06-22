fbd5d0614fdb3738d04ccb7a3addc21081417c97
Elson Correia <design@elsoncorreia.com>
2021-06-22 04:25:20 -0400

    code snippet+highlightjs

--- website/tags/code-snippet.js
+++ website/tags/code-snippet.js
@@ -1,65 +1,33 @@
-const commandsPattern = /npm|node|npx|cd|mkdir|touch|ls/gm;
-const varsPattern = /\s(var|let|const)\s/gm;
-const importPattern = /require|import|export|module\.exports/gm;
-const jsKeywordsPattern = /\s(function|class|switch|if|while|for|break|continue\.exports|__dirname|__filename)\s/gm;
-const objPattern = /Array|Object|Map|Set|Symbol\.exports/gm;
-const propertyPattern = /\.[\w\-_]+/gm;
-const commentPattern = /#.+/gm
+const hljs = require('highlight.js');
 
 class CodeSnippet {
-  style = `
-    overflow: scroll;
-    background: #212f36;
-    border-radius: 5px;
-    color: #fff;
-    padding: 20px 25px;
-    line-height: 130%;
+  preStyle = `
+    padding: 20px 30px;
+    display: block;
+    overflow: auto;
     font-size: 0.9rem;
     font-weight: 200;
-    margin-bottom: 35px;
+    border-radius: 5px;
     text-align: left;
-    white-space: pre-line;
-    tab-width: 4;
+    line-height: 135%;
+    tab-size: 2;
   `.replace(/\s{2}/g, '');
   
   constructor(node) {
     this.node = node;
-    const {type} = node.attributes;
+    const {language = 'bash'} = node.attributes;
     let content = this.node.innerHTML.trim();
     
-    switch (type) {
-      case 'js':
-        content = content
-          .replace(varsPattern, (m) => `<span style="color: #f072ff">${m}</span>`)
-          .replace(importPattern, (m) => `<span style="color: #11d0ad">${m}</span>`)
-          .replace(propertyPattern, (m) => `<span style="color: #ffc107">${m}</span>`)
-          .replace(jsKeywordsPattern, (m) => `<span style="color: #2eefdf">${m}</span>`)
-          .replace(/</g, '&lt;')
-          .replace(/>/g, '&gt;');
-        break;
-      case 'html':
-        content = content
-          .replace(/</g, '&lt;')
-          .replace(/>/g, '&gt;');
-        break;
-      case 'css':
-        break;
-      case 'terminal':
-      case 'bash':
-        content = content
-          .replace(commandsPattern, (m) => `<span style="color: #2fe99e">${m}</span>`);
-        break;
-      default:
-    }
-    
-    this.content = content
-      .replace(/@beforesemicolon\/html-plus/g, (m) => `<span style="color: #feffa2">${m}</span>`)
-      .replace(/express/g, (m) => `<span style="color: #feffa2">${m}</span>`)
-      .replace(/engine/g, (m) => `<span style="color: #feffa2">${m}</span>`)
+    this.content = hljs.highlight(content, {language}).value;
+    this.language = language;
   }
   
   render() {
-    return `<pre style="${this.style}"><code>${this.content}</code></pre>`;
+    return '<pre class="hljs" style="' + this.preStyle + '">' +
+      '<code class="language-' + this.language + '">' +
+      this.content +
+      '</code>' +
+      '</pre>';
   }
 }
 
