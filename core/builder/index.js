5485de3d37e0114c2616f5b9482ce933c3b27a96
Elson Correia <design@elsoncorreia.com>
2021-07-03 14:07:09 -0400

    dir building for css handling only

--- core/builder/index.js
+++ core/builder/index.js
@@ -2,9 +2,8 @@ const {File} = require('./../File');
 const fs = require('fs');
 const {mkdir, rmdir, copyFile, writeFile} = require('fs/promises');
 const path = require('path');
-const chalk = require("chalk");
+const {PartialFile} = require("../PartialFile");
 const {uniqueAlphaNumericId} = require("../utils/unique-alpha-numeric-id");
-const {extractPartials} = require("./extract-partials");
 const {getDirectoryFilesDetail} = require("../utils/getDirectoryFilesDetail");
 const {transform} = require("../transform");
 const {transform: transformer} = require("../transformers/index");
@@ -20,7 +19,9 @@ const defaultOptions = {
   customTags: [],
   customAttributes: []
 };
-let sourcePaths = {};
+let resources = {};
+let partials = [];
+let pages = [];
 
 async function build(options = defaultOptions) {
   options = {...defaultOptions, ...options, env: 'production'};
@@ -32,48 +33,59 @@ async function build(options = defaultOptions) {
   if (!fs.existsSync(options.srcDir)) {
     throw new Error('Source directory not found: ' + options.srcDir)
   }
+  
+  resources = {};
+  partials = [];
+  pages = [];
+  
   console.time('\nDone');
-  return getDirectoryFilesDetail(options.srcDir, 'html')
-    .then(async files => {
-      console.log('-- files', files);
-      // options.partialFiles = extractPartials(files, options.srcDir);
-      // let pages;
-      // sourcePaths = {};
-      //
-      // if (fs.existsSync(options.destDir)) {
-      //   await rmdir(options.destDir, {recursive: true});
-      // }
-      //
-      // await mkdir(options.destDir);
-      // await mkdir(path.join(options.destDir, 'stylesheets'));
-      // await mkdir(path.join(options.destDir, 'scripts'));
-      // await mkdir(path.join(options.destDir, 'assets'));
-      //
-      // if (options.template) {
-      //   if (options.templateContextDataList.length) {
-      //     console.log(`Building template ${chalk.green(options.template)} for ${chalk.greenBright(options.templateContextDataList.length)} data entries...`);
-      //     return await Promise.all(options.templateContextDataList.map(async ([fileName, contextData]) => {
-      //       return savePageAndResources(await processPage(options.template, fileName, {
-      //         ...options,
-      //         contextData,
-      //       }), options);
-      //     }));
-      //   } else {
-      //     return savePageAndResources(await processPage(options.template, path.basename(options.template), {
-      //       ...options,
-      //       get contextData() {
-      //         return typeof options.contextDataProvider === 'function'
-      //           ? options.contextDataProvider(options.template)
-      //           : {};
-      //       },
-      //     }), options);
-      //   }
-      // } else {
-      //   // check if source directory exists
-      //   // read all html files inside the source directory
-      // }
-    })
-    .then(() => {
+  return getDirectoryFilesDetail(options.srcDir, collectFilePaths(options.destDir))
+    .then(async () => {
+      // clear previous destination directory
+      if (fs.existsSync(options.destDir)) {
+        await rmdir(options.destDir, {recursive: true});
+      }
+  
+      // create destination directory with all essential subdirectories
+      await mkdir(options.destDir);
+      await mkdir(path.join(options.destDir, 'stylesheets'));
+      await mkdir(path.join(options.destDir, 'scripts'));
+      await mkdir(path.join(options.destDir, 'assets'));
+      
+      const pageResources = {};
+      
+      await Promise.all(
+        pages.map(async page => {
+          const pageRoutePath = page
+            .replace(options.srcDir, '')
+            .replace(/\/index\.html$/, '')
+            .replace(/\.html$/, '') || '/';
+    
+          const contextData = typeof options.contextDataProvider === 'function'
+            ? options.contextDataProvider({file: page, path: pageRoutePath})
+            : {}
+    
+          const {content, resources, file} = processPage(page, path.basename(page), {...options, contextData});
+    
+          resources.forEach(rsc => {
+            pageResources[rsc.srcPath] = rsc;
+          })
+    
+          const pageDestPath = path.join(options.destDir, file.filePath)
+    
+          await mkdir(path.join(
+            options.destDir,
+            file.fileDirectoryPath.replace(options.srcDir, '')
+          ), {recursive: true});
+    
+          await writeFile(pageDestPath, content.replace(/\n|\s{2,}/g, ''));
+        })
+      )
+      
+      await Promise.all(
+        Object.values(pageResources).map(resource => processPageResource(resource, options.destDir))
+      )
+      
       console.timeEnd('\nDone');
     })
     .catch(async e => {
@@ -82,28 +94,49 @@ async function build(options = defaultOptions) {
     })
 }
 
-async function processPage(pagePath, fileName, opt) {
+function collectFilePaths(pagesDirectoryPath) {
+  return (filePath) => {
+    if (/\.html$/.test(filePath)) {
+      const fileName = path.basename(filePath);
+      
+      if (fileName.startsWith('_')) {
+        partials.push(new PartialFile(filePath, pagesDirectoryPath));
+      } else {
+        pages.push(filePath)
+      }
+    } else if(!path.basename(filePath).startsWith('_')) {
+      resources[filePath] = {
+        path: filePath,
+        hash: uniqueAlphaNumericId(8)
+      }
+    }
+    
+    return false;
+  }
+}
+
+function processPage(pagePath, fileName, opt) {
   const resources = [];
   const file = new File(pagePath, opt.srcDir);
   const fileExportName = pagePath.replace(path.basename(pagePath), fileName);
-  
+
   const content = transform(file.toString(), {
     file,
     data: opt.staticData,
     context: opt.contextData,
     customTags: opt.customTags,
     customAttributes: opt.customAttributes,
-    partialFiles: opt.partialFiles,
+    partialFiles: partials,
     env: opt.env,
     onBeforeRender: (node, nodeFile) => {
       const resource = collectAndUpdateNodeSourceLink(node, nodeFile);
-      
+
       if (resource) {
         resources.push(resource);
       }
     }
   });
-  
+
   return {
     content,
     resources,
@@ -114,7 +147,7 @@ async function processPage(pagePath, fileName, opt) {
 function collectAndUpdateNodeSourceLink(node, nodeFile) {
   let srcPath = '';
   let srcAttrName = '';
-  
+
   switch (node.tagName) {
     case 'link':
     case 'area':
@@ -148,119 +181,100 @@ function collectAndUpdateNodeSourceLink(node, nodeFile) {
       break;
     default:
   }
-  
+
   let isURL = false;
-  
+
   try {
     new URL(srcPath);
     isURL = true;
   } catch (e) {
   }
-  
+
   if (srcPath && typeof srcPath === 'string' && !isURL) {
-    
+
     if (/node_modules\//.test(srcPath)) {
       srcPath = srcPath.replace(/^.+(?=node_modules)/, `${process.cwd()}/`);
     } else {
       srcPath = path.resolve(nodeFile.fileDirectoryPath, srcPath);
     }
     
-    let srcDestPath = '';
-    
-    if (sourcePaths[srcPath]) {
-      srcDestPath = sourcePaths[srcPath];
-    } else {
-      srcDestPath = getFileSourceHashedDestPath(srcPath);
-      sourcePaths[srcPath] = srcDestPath;
+    if (!resources[srcPath]) {
+      resources[srcPath] = {
+        path: srcPath,
+        hash: uniqueAlphaNumericId(8)
+      }
     }
-    
-    const relativePath = path.relative(nodeFile.fileDirectoryPath, nodeFile.srcDirectoryPath);
+
+    const srcDestPath = getFileSourceHashedDestPath(srcPath, resources[srcPath].hash);
+    const relativePath = path.relative(nodeFile.fileDirectoryPath, nodeFile.srcDirectoryPath) || '.';
     node.setAttribute(srcAttrName, `${relativePath}/${srcDestPath}`);
-    
-    return {srcPath, srcDestPath};
+
+    return {srcPath, srcDestPath, pageFile: nodeFile};
   }
-  
+
   return null;
 }
 
-async function savePageAndResources(pg, options) {
-  const pagePath = path.join(options.destDir, pg.file.filePath)
-  
-  await mkdir(path.join(
-    options.destDir,
-    pg.file.fileDirectoryPath.replace(options.srcDir, '')
-  ), {recursive: true});
-  
-  await writeFile(pagePath, pg.content.replace(/\n|\s{2,}/g, ''));
-  
-  await Promise.all(
-    pg.resources.map(resource => processPageResource(resource, options.destDir, new File(pagePath, options.destDir)))
-  );
-}
-
-async function processPageResource({srcPath, srcDestPath}, destPath, pageFile) {
+async function processPageResource({srcPath, srcDestPath, pageFile}, destPath) {
   const env = 'production';
   const ext = path.extname(srcPath);
   const absoluteDestPath = path.join(destPath, srcDestPath);
-  let content = null;
+  const file = new File(srcPath, pageFile.srcDirectoryPath);
+  const assetsPath = `../assets`;
+  const assetsHashedMap = resources;
+  let content;
+  let linkedResources;
   
-  if (!fs.existsSync(absoluteDestPath)) {
-    
-    switch (ext) {
-      case '.sass':
-      case '.scss':
-        content = await transformer.sass({file: new File(srcPath, pageFile.srcDirectoryPath)});
-        content = await transformer.css(content, {
-          pageFile,
-          destPath,
-          env,
-          file: new File(absoluteDestPath, pageFile.srcDirectoryPath)
-        })
-        break;
-      case '.less':
-        content = await transformer.less({file: new File(srcPath)});
-        content = await transformer.css(content, {
-          pageFile,
-          destPath,
-          env,
-          file: new File(absoluteDestPath, pageFile.srcDirectoryPath)
-        })
-        break;
-      case '.styl':
-        content = await transformer.stylus({file: new File(srcPath)});
-        content = await transformer.css(content, {
-          pageFile,
-          destPath,
-          env,
-          file: new File(absoluteDestPath, pageFile.srcDirectoryPath)
-        })
-        break;
-      case '.css':
-        content = await transformer.css({pageFile, destPath, env, file: new File(srcPath, pageFile.srcDirectoryPath)})
-        break;
-      case '.js':
-      case '.mjs':
-      case '.ts':
-      case '.jsx':
-      case '.tsx':
-        content = await transformer.js({env, file: new File(srcPath, pageFile.srcDirectoryPath)})
-        break;
-      default:
-        await copyFile(srcPath, absoluteDestPath)
+  switch (ext) {
+    case '.sass':
+    case '.scss':
+      content = await transformer.sass({file: new File(srcPath, pageFile.srcDirectoryPath)});
+      ({content, linkedResources} = await transformer.css(content, {pageFile, destPath, env, file, assetsPath, assetsHashedMap}));
+      break;
+    case '.less':
+      content = await transformer.less({file: new File(srcPath)});
+      ({content, linkedResources} = await transformer.css(content, {pageFile, destPath, env, file, assetsPath, assetsHashedMap}))
+      break;
+    case '.styl':
+      content = await transformer.stylus({file: new File(srcPath)});
+      ({content, linkedResources} = await transformer.css(content, {pageFile, destPath, env, file, assetsPath, assetsHashedMap}))
+      break;
+    case '.css':
+      ({content, linkedResources} = await transformer.css({pageFile, destPath, env, file, assetsPath, assetsHashedMap}))
+      break;
+    // case '.js':
+    // case '.mjs':
+    // case '.ts':
+    // case '.jsx':
+    // case '.tsx':
+    //   content = await transformer.js({env, file: new File(srcPath, pageFile.srcDirectoryPath)})
+    //   break;
+    default:
+      await copyFile(srcPath, absoluteDestPath)
+  }
+
+  if (typeof content === 'string') {
+    await writeFile(absoluteDestPath, content);
+  
+    for (let source of linkedResources) {
+      if (!resources[source].copied) {
+        const srcDestPath = path.join(destPath, getFileSourceHashedDestPath(source, resources[source].hash));
+        resources[source].copied = true;
+
+        try {
+          await copyFile(source, srcDestPath).catch();
+        } catch(e) {
+          console.error(`Asset not found: ${source} in ${srcPath}. This can be due to nested imports with relative path to assets.`);
+        }
+      }
     }
-    
-    if (typeof content === 'string') {
-      await writeFile(absoluteDestPath, content);
-    }
-    
   }
 }
 
-function getFileSourceHashedDestPath(src) {
+function getFileSourceHashedDestPath(src, hash) {
   let fileDestPath = '';
-  const hash = uniqueAlphaNumericId(16);
   const ext = path.extname(src);
-  
+
   switch (ext) {
     case '.sass':
     case '.scss':
@@ -277,10 +291,11 @@ function getFileSourceHashedDestPath(src) {
       break;
     case '.js':
     case '.mjs':
+    case '.cjs':
     case '.ts':
     case '.jsx':
     case '.tsx':
-      fileDestPath = path.join('scripts', path.basename(src).replace(/\.ts|tsx|jsx$/, `-${hash}.js`))
+      fileDestPath = path.join('scripts', path.basename(src).replace(/\.(?:c|m)?(?:t|j)sx?$/, `-${hash}.js`))
       break;
     default:
       fileDestPath = path.join('assets', path.basename(src).replace(/\.[a-zA-Z0-9]{2,}$/, `-${hash}${ext}`))
