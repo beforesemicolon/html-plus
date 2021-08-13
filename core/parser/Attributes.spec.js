4a0a73ea4647c62e06518e2fc036a8109dc422a4
Elson Correia <design@elsoncorreia.com>
2021-08-12 21:39:21 -0400

    Attributes test

--- core/parser/Attributes.spec.js
+++ core/parser/Attributes.spec.js
@@ -1,5 +1,36 @@
+const {Attributes} = require('./Attributes');
+const {Attr} = require('./Attr');
+
 describe('Attributes', () => {
-  it('should set', () => {});
-  it('should get length', () => {});
-  it('should get attribute by name', () => {});
+  const attributes = new Attributes('#if="shouldShow" href="sample/path" download');
+  
+  it('should set', () => {
+    expect(attributes.toString()).toBe('#if="shouldShow" href="sample/path" download')
+  });
+  
+  it('should be iterable', () => {
+    const testCases = [
+      ['if', 'shouldShow'],
+      ['href', 'sample/path'],
+      ['download', null],
+    ];
+  
+    let i = 0;
+    for (let attr of attributes) {
+      expect(attr).toBeInstanceOf(Attr);
+      expect(attr.name).toBe(testCases[i][0]);
+      expect(attr.value).toBe(testCases[i][1]);
+      i += 1;
+    }
+  });
+  
+  it('should get length', () => {
+    expect(attributes.length).toBe(3)
+  });
+  
+  it('should get attribute by name', () => {
+    expect(attributes.getNamedItem('if')).toBeInstanceOf(Attr);
+    expect(attributes.getNamedItem('download').value).toEqual(null);
+    expect(attributes.getNamedItem('href').value).toEqual('sample/path');
+  });
 });
