093f75348f4d6a10fd45d2e78ef0d63b47d632da
Elson Correia <design@elsoncorreia.com>
2021-09-25 02:15:19 -0400

    improve create selector util

--- core/parser/utils/createSelectors.js
+++ core/parser/utils/createSelectors.js
@@ -1,103 +1,110 @@
-const {selectors, pseudoClass} = require('./regexPatterns');
+const {Selector} = require("../Selector");
 
-class Selector{
-   operator = null;
-   modifier = null;
-   
-   constructor(type, name = null, value = null) {
-      this.type = type;
-      this.name = name;
-      this.value = value;
-   }
-}
-
+/**
+ * creates a selector list from the CSS selector String.
+ * All selectors will be an instance of Selector class
+ * @param selectorString
+ * @returns Array<Selector>
+ */
 function createSelectors(selectorString) {
-   selectorString = selectorString.trim();
-   
-   if (/^(\+|\~|\>)/.test(selectorString)) {
-       throw new Error('Invalid selector string: It may not start with any combinator symbol.')
-   }
-   
-   if (/(\+|\~|\>)$/.test(selectorString)) {
-      throw new Error('Invalid selector string: It may not end with any combinator symbol.')
-   }
-   
-   let selectorsList = [];
-   let match;
-   let lastSelector = null;
-   selectors.lastIndex = 0
-   
-   while((match = selectors.exec(selectorString)) !== null) {
-      let selector = null;
-      
-      switch (true) {
-         case match[0].startsWith('#'):
-            selector = new Selector('attribute', 'id', match[8]);
-            break;
-         case match[0].startsWith('.'):
-            selector = new Selector('attribute', 'class', match[8]);
-            break;
-         case match[0].startsWith(':') && pseudoClass.test(match[0]):
-            selector = new Selector('pseudo-class', match[2], match[3]);
-            break;
-         case match[0].startsWith('[') && match[0].endsWith(']'):
-            selector = new Selector('attribute', match[2] || match[4], match[6]);
-            
-            if (match[5]) {
-               selector.operator = match[5];
-            }
-   
-            if (match[7]) {
-               selector.modifier = match[7];
-            }
-            
-            break;
-         case match[0] === '*':
-            selector = new Selector('global', null, '*');
-            break;
-         case /^\s+$/.test(match[0]):
-            selector = new Selector('combinator', null, ' ');
-            break;
-         case match[1] === '+':
-         case match[1] === '~':
-         case match[1] === '>':
-            selector = new Selector('combinator', null, match[1]);
-            break;
-         default:
-            if (match[8] && /[a-z][\w-]*/i.test(match[8])) {
-               selector = new Selector('tag', match[8]);
-            }
-      }
-      
-      if (selector) {
-         if (lastSelector && lastSelector.type === 'combinator' && lastSelector.type === selector.type) {
-            throw new Error('Invalid selector string: Must not contain nested combinator symbols.')
-         }
-         
-         selectorsList.push(selector);
-         lastSelector = selector;
-      } else {
-         /**
-          * if any selector fails, the whole selector is invalid
-          * @type {*[]}
-          */
-         selectorsList = [];
-         break;
-      }
-   }
-   
-   return selectorsList
-     .reduce(({selectors, lastSelector}, sel) => {
-        if (!selectors.length) {
-           selectors = [[sel]];
-        } else if (sel.type === 'combinator' || lastSelector.type === 'combinator') {
-           selectors = [...selectors, [sel]];
+  selectorString = selectorString.trim();
+  
+  if (/^(\+|\~|\>)/.test(selectorString)) {
+    throw new Error('Invalid selector string: It may not start with any combinator symbol.')
+  }
+  
+  if (/(\+|\~|\>)$/.test(selectorString)) {
+    throw new Error('Invalid selector string: It may not end with any combinator symbol.')
+  }
+  
+  const selectors = /(\+|\~|\>|\*)|:([a-z][a-z-]*)(?:\(([^)]*)\))?|\[\s*([a-z][\w:.-]*)\s*(?:\s*(\*|\||\^|\$|\~)?\s*=\s*"([^"]*)")?\s*(i|s)?\s*\]|(?:#|\.)?([a-z][\w-]*)|(?<=[^>+~\s])([ \t]+)(?=[^>+~\s])/gi;
+  const pseudoClass = /:(root|not|disabled|enabled|checked|read-only|read-write|required|optional|empty|nth-last-child|nth-child|first-child|last-child|only-child|nth-of-type|nth-last-of-type|first-of-type|last-of-type|only-of-type)/;
+  let selectorsList = [];
+  let match;
+  let lastSelector = null;
+  selectors.lastIndex = 0
+  
+  while ((match = selectors.exec(selectorString)) !== null) {
+    let selector = null;
+    
+    switch (true) {
+      case match[0].startsWith('#'):
+        selector = Selector.id(match[8]);
+        break;
+      case match[0].startsWith('.'):
+        selector = Selector.class(match[8]);
+        break;
+      case match[0].startsWith(':') && pseudoClass.test(match[0]):
+        const name = match[2];
+        const value = match[3];
+        
+        if (name === 'not') {
+          if (/:not/g.test(value) || /,/g.test(value)) {
+            selector = null;
+          } else {
+            selector = Selector.pseudoClass(name, createSelectors(value).flat());
+          }
         } else {
-           selectors[selectors.length - 1].push(sel)
+          selector = Selector.pseudoClass(name, value);
         }
-     
-        return {selectors, lastSelector: sel};
-     }, {selectors: [], lastSelector: null});
+        
+        break;
+      case match[0].startsWith('[') && match[0].endsWith(']'):
+        selector = Selector.attribute(match[2] || match[4], match[6]);
+        
+        if (match[5]) {
+          selector.operator = match[5];
+        }
+        
+        if (match[7]) {
+          selector.modifier = match[7];
+        }
+        
+        break;
+      case match[0] === '*':
+        selector = Selector.global();
+        break;
+      case /^\s+$/.test(match[0]):
+        selector = Selector.combinator(' ');
+        break;
+      case match[1] === '+':
+      case match[1] === '~':
+      case match[1] === '>':
+        selector = Selector.combinator(match[1]);
+        break;
+      default:
+        if (match[8] && /[a-z][\w-]*/i.test(match[8])) {
+          selector = Selector.tag(match[8]);
+        }
+    }
+    
+    if (selector) {
+      if (lastSelector && lastSelector.type === 'combinator' && lastSelector.type === selector.type) {
+        throw new Error('Invalid selector string: Must not contain nested combinator symbols.')
+      }
+      
+      selectorsList.push(selector);
+      lastSelector = selector;
+    } else {
+      // if any selector fails, the whole selector is invalid
+      // so the selector list becomes empty and we quit the loop
+      selectorsList = [];
+      break;
+    }
+  }
+  
+  return selectorsList
+    .reduce(({selectors, lastSelector}, sel) => {
+      if (!selectors.length) {
+        selectors = [[sel]];
+      } else if (sel.type === 'combinator' || lastSelector.type === 'combinator') {
+        selectors = [...selectors, [sel]];
+      } else {
+        selectors[selectors.length - 1].push(sel)
+      }
+      
+      return {selectors, lastSelector: sel};
+    }, {selectors: [], lastSelector: null}).selectors;
 }
 
 module.exports.createSelectors = createSelectors;
--- core/parser/utils/createSelectors.spec.js
+++ core/parser/utils/createSelectors.spec.js
@@ -2,27 +2,31 @@ const {createSelectors} = require("./createSelectors");
 
 describe('createSelectors', () => {
   it('global selector', () => {
-    expect(createSelectors('*')).toEqual([
-      {
-        "modifier": null,
-        "name": null,
-        "operator": null,
-        "type": "global",
-        "value": null
-      }
-    ])
+    expect(createSelectors('*')).toEqual(expect.arrayContaining([
+      [
+        {
+          "modifier": null,
+          "name": null,
+          "operator": null,
+          "type": "global",
+          "value": "*"
+        }
+      ]
+    ]))
   });
   
   it('tag name selector', () => {
-    expect(createSelectors('section')).toEqual([
-      {
-        "modifier": null,
-        "name": 'section',
-        "operator": null,
-        "type": "tag",
-        "value": null
-      }
-    ])
+    expect(createSelectors('section')).toEqual(expect.arrayContaining([
+      [
+        {
+          "modifier": null,
+          "name": 'section',
+          "operator": null,
+          "type": "tag",
+          "value": null
+        }
+      ]
+    ]))
   });
   
   describe('should throw error', () => {
@@ -62,325 +66,401 @@ describe('createSelectors', () => {
   
   describe('attribute selectors', () => {
     it('id', () => {
-      expect(createSelectors('#sample')).toEqual([
-        {
-          "modifier": null,
-          "name": "id",
-          "operator": null,
-          "type": "attribute",
-          "value": "sample"
-        }
-      ])
+      expect(createSelectors('#sample')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": "id",
+            "operator": null,
+            "type": "attribute",
+            "value": "sample"
+          }
+        ]
+      ]))
       expect(createSelectors('#')).toEqual([])
     });
   
     it('class', () => {
-      expect(createSelectors('.sample')).toEqual([
-        {
-          "modifier": null,
-          "name": "class",
-          "operator": null,
-          "type": "attribute",
-          "value": "sample"
-        }
-      ])
+      expect(createSelectors('.sample')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": "class",
+            "operator": null,
+            "type": "attribute",
+            "value": "sample"
+          }
+        ]
+      ]))
       expect(createSelectors('.')).toEqual([])
     });
     
     it('attribute name', () => {
-      expect(createSelectors('[href]')).toEqual([
-        {
-          "modifier": null,
-          "name": "href",
-          "operator": null,
-          "type": "attribute",
-          "value": null
-        }
-      ])
+      expect(createSelectors('[href]')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": "href",
+            "operator": null,
+            "type": "attribute",
+            "value": null
+          }
+        ]
+      ]))
     });
   
     it('attribute name and modifier', () => {
-      expect(createSelectors('[href i]')).toEqual([
-        {
-          "modifier": "i",
-          "name": "href",
-          "operator": null,
-          "type": "attribute",
-          "value": null
-        }
-      ])
+      expect(createSelectors('[href i]')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": "i",
+            "name": "href",
+            "operator": null,
+            "type": "attribute",
+            "value": null
+          }
+        ]
+      ]))
     });
   
     it('attribute name value', () => {
-      expect(createSelectors('[name="email"]')).toEqual([
-        {
-          "modifier": null,
-          "name": "name",
-          "operator": null,
-          "type": "attribute",
-          "value": "email"
-        }
-      ])
+      expect(createSelectors('[name="email"]')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": "name",
+            "operator": null,
+            "type": "attribute",
+            "value": "email"
+          }
+        ]
+      ]))
     });
   
     it('attribute name value with operator', () => {
-      expect(createSelectors('[href^="/some/path"]')).toEqual([
-        {
-          "modifier": null,
-          "name": "href",
-          "operator": "^",
-          "type": "attribute",
-          "value": "/some/path"
-        }
-      ]);
+      expect(createSelectors('[href^="/some/path"]')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": "href",
+            "operator": "^",
+            "type": "attribute",
+            "value": "/some/path"
+          }
+        ]
+      ]));
     });
   
     it('attribute name value with modifier', () => {
-      expect(createSelectors('[href="/some/path" i]')).toEqual([
-        {
-          "modifier": "i",
-          "name": "href",
-          "operator": null,
-          "type": "attribute",
-          "value": "/some/path"
-        }
-      ])
+      expect(createSelectors('[href="/some/path" i]')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": "i",
+            "name": "href",
+            "operator": null,
+            "type": "attribute",
+            "value": "/some/path"
+          }
+        ]
+      ]))
     });
   
     it('attribute name value with operator and modifier', () => {
-      expect(createSelectors('[href$="/some/path" i]')).toEqual([
-        {
-          "modifier": "i",
-          "name": "href",
-          "operator": "$",
-          "type": "attribute",
-          "value": "/some/path"
-        }
-      ])
+      expect(createSelectors('[href$="/some/path" i]')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": "i",
+            "name": "href",
+            "operator": "$",
+            "type": "attribute",
+            "value": "/some/path"
+          }
+        ]
+      ]))
     });
   });
   
   describe('combinators', () => {
+    
     it('next sibling', () => {
-      expect(createSelectors(' + ')).toEqual([
-        {
-          "modifier": null,
-          "name": null,
-          "operator": null,
-          "type": "combinator",
-          "value": "+"
-        }
-      ])
+      expect(createSelectors('div + p')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": null,
+            "operator": null,
+            "type": "combinator",
+            "value": "+"
+          }
+        ],
+      ]))
     });
   
     it('adjacent siblings', () => {
-      expect(createSelectors(' ~ ')).toEqual([
-        {
-          "modifier": null,
-          "name": null,
-          "operator": null,
-          "type": "sibling",
-          "value": "~"
-        }
-      ])
+      expect(createSelectors('div ~ p')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": null,
+            "operator": null,
+            "type": "combinator",
+            "value": "~"
+          }
+        ],
+      ]))
     });
   
     it('direct child', () => {
-      expect(createSelectors(' > ')).toEqual([
-        {
-          "modifier": null,
-          "name": null,
-          "operator": null,
-          "type": "descendent",
-          "value": ">"
-        }
-      ])
+      expect(createSelectors('div > p')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": null,
+            "operator": null,
+            "type": "combinator",
+            "value": ">"
+          }
+        ]
+      ]))
     });
   
     it('descendents', () => {
-      expect(createSelectors(' ')).toEqual([
-        {
-          "modifier": null,
-          "name": null,
-          "operator": null,
-          "type": "descendent",
-          "value": null
-        }
-      ])
+      expect(createSelectors('div p')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": null,
+            "operator": null,
+            "type": "combinator",
+            "value": " "
+          }
+        ]
+      ]))
     });
   });
   
   describe('pseudo-class selector', () => {
     it('disabled & enabled', () => {
-      expect(createSelectors(':disabled')).toEqual([
-        {
-          "modifier": null,
-          "name": 'disabled',
-          "operator": null,
-          "type": "pseudo-class",
-          "value": null
-        }
-      ])
-      expect(createSelectors(':enabled')).toEqual([
-        {
-          "modifier": null,
-          "name": 'enabled',
-          "operator": null,
-          "type": "pseudo-class",
-          "value": null
-        }
-      ])
+      expect(createSelectors(':disabled')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": 'disabled',
+            "operator": null,
+            "type": "pseudo-class",
+            "value": null
+          }
+        ]
+      ]))
+      expect(createSelectors(':enabled')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": 'enabled',
+            "operator": null,
+            "type": "pseudo-class",
+            "value": null
+          }
+        ]
+      ]))
     });
   
-    it('blank & empty', () => {
-      expect(createSelectors(':blank')).toEqual([
-        {
-          "modifier": null,
-          "name": 'blank',
-          "operator": null,
-          "type": "pseudo-class",
-          "value": null
-        }
-      ])
-      expect(createSelectors(':empty')).toEqual([
-        {
-          "modifier": null,
-          "name": 'empty',
-          "operator": null,
-          "type": "pseudo-class",
-          "value": null
-        }
-      ])
+    it('empty', () => {
+      expect(createSelectors(':empty')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": 'empty',
+            "operator": null,
+            "type": "pseudo-class",
+            "value": null
+          }
+        ]
+      ]))
     });
   
     it('optional & required', () => {
-      expect(createSelectors(':optional')).toEqual([
-        {
-          "modifier": null,
-          "name": 'optional',
-          "operator": null,
-          "type": "pseudo-class",
-          "value": null
-        }
-      ])
-      expect(createSelectors(':required')).toEqual([
-        {
-          "modifier": null,
-          "name": 'required',
-          "operator": null,
-          "type": "pseudo-class",
-          "value": null
-        }
-      ])
+      expect(createSelectors(':optional')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": 'optional',
+            "operator": null,
+            "type": "pseudo-class",
+            "value": null
+          }
+        ]
+      ]))
+      expect(createSelectors(':required')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": 'required',
+            "operator": null,
+            "type": "pseudo-class",
+            "value": null
+          }
+        ]
+      ]))
     });
   
     it('read-only & read-write', () => {
-      expect(createSelectors(':read-only')).toEqual([
-        {
-          "modifier": null,
-          "name": 'read-only',
-          "operator": null,
-          "type": "pseudo-class",
-          "value": null
-        }
-      ])
-      expect(createSelectors(':read-write')).toEqual([
-        {
-          "modifier": null,
-          "name": 'read-write',
-          "operator": null,
-          "type": "pseudo-class",
-          "value": null
-        }
-      ])
+      expect(createSelectors(':read-only')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": 'read-only',
+            "operator": null,
+            "type": "pseudo-class",
+            "value": null
+          }
+        ]
+      ]))
+      expect(createSelectors(':read-write')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": 'read-write',
+            "operator": null,
+            "type": "pseudo-class",
+            "value": null
+          }
+        ]
+      ]))
     });
   
     it('nth-child & first-child & last-child & only-child', () => {
-      expect(createSelectors(':nth-child(2)')).toEqual([
-        {
-          "modifier": null,
-          "name": 'nth-child',
-          "operator": null,
-          "type": "pseudo-class",
-          "value": "2"
-        }
-      ])
-      
-      expect(createSelectors(':first-child')).toEqual([
-        {
-          "modifier": null,
-          "name": 'first-child',
-          "operator": null,
-          "type": "pseudo-class",
-          "value": null
-        }
-      ])
-      expect(createSelectors(':last-child')).toEqual([
-        {
-          "modifier": null,
-          "name": 'last-child',
-          "operator": null,
-          "type": "pseudo-class",
-          "value": null
-        }
-      ])
-      expect(createSelectors(':only-child')).toEqual([
-        {
-          "modifier": null,
-          "name": 'only-child',
-          "operator": null,
-          "type": "pseudo-class",
-          "value": null
-        }
-      ])
+      expect(createSelectors(':nth-child(2)')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": 'nth-child',
+            "operator": null,
+            "type": "pseudo-class",
+            "value": "2"
+          }
+        ]
+      ]))
+      expect(createSelectors(':first-child')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": 'first-child',
+            "operator": null,
+            "type": "pseudo-class",
+            "value": null
+          }
+        ]
+      ]))
+      expect(createSelectors(':last-child')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": 'last-child',
+            "operator": null,
+            "type": "pseudo-class",
+            "value": null
+          }
+        ]
+      ]))
+      expect(createSelectors(':only-child')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": 'only-child',
+            "operator": null,
+            "type": "pseudo-class",
+            "value": null
+          }
+        ]
+      ]))
     });
   
     it('nth-of-type & nth-last-of-type & first-of-type & last-of-type & only-of-type', () => {
-      expect(createSelectors(':nth-of-type(2n+1)')).toEqual([
-        {
-          "modifier": null,
-          "name": 'nth-of-type',
-          "operator": null,
-          "type": "pseudo-class",
-          "value": "2n+1"
-        }
-      ])
-      expect(createSelectors(':nth-last-of-type(2n)')).toEqual([
-        {
-          "modifier": null,
-          "name": 'nth-last-of-type',
-          "operator": null,
-          "type": "pseudo-class",
-          "value": "2n"
-        }
-      ])
-      expect(createSelectors(':first-of-type')).toEqual([
-        {
-          "modifier": null,
-          "name": 'first-of-type',
-          "operator": null,
-          "type": "pseudo-class",
-          "value": null
-        }
-      ])
-      expect(createSelectors(':last-of-type')).toEqual([
-        {
-          "modifier": null,
-          "name": 'last-of-type',
-          "operator": null,
-          "type": "pseudo-class",
-          "value": null
-        }
-      ])
-      expect(createSelectors(':only-of-type')).toEqual([
-        {
-          "modifier": null,
-          "name": 'only-of-type',
-          "operator": null,
-          "type": "pseudo-class",
-          "value": null
-        }
-      ])
+      expect(createSelectors(':nth-of-type(2n+1)')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": 'nth-of-type',
+            "operator": null,
+            "type": "pseudo-class",
+            "value": "2n+1"
+          }
+        ]
+      ]))
+      expect(createSelectors(':nth-last-of-type(2n)')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": 'nth-last-of-type',
+            "operator": null,
+            "type": "pseudo-class",
+            "value": "2n"
+          }
+        ]
+      ]))
+      expect(createSelectors(':first-of-type')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": 'first-of-type',
+            "operator": null,
+            "type": "pseudo-class",
+            "value": null
+          }
+        ]
+      ]))
+      expect(createSelectors(':last-of-type')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": 'last-of-type',
+            "operator": null,
+            "type": "pseudo-class",
+            "value": null
+          }
+        ]
+      ]))
+      expect(createSelectors(':only-of-type')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": 'only-of-type',
+            "operator": null,
+            "type": "pseudo-class",
+            "value": null
+          }
+        ]
+      ]))
+    });
+  
+    it('not', () => {
+      expect(createSelectors(':not(.box.sample)')).toEqual(expect.arrayContaining([
+        [
+          {
+            "modifier": null,
+            "name": "not",
+            "operator": null,
+            "type": "pseudo-class",
+            "value": [
+              {
+                "modifier": null,
+                "name": "class",
+                "operator": null,
+                "type": "attribute",
+                "value": "box"
+              },
+              {
+                "modifier": null,
+                "name": "class",
+                "operator": null,
+                "type": "attribute",
+                "value": "sample"
+              }
+            ]
+          }
+        ]
+      ]))
     });
   });
   
@@ -390,10 +470,11 @@ describe('createSelectors', () => {
       ['button:disabled > span:only-child', ["button", "disabled", ">", "span", "only-child"]],
       ['section#box h2', ["section", "box", " ", "h2"]],
       ['ul > li.item', ["ul", ">", "li", "item"]],
-      ['*:disabled', [null, "disabled"]],
+      ['*:disabled', ['*', "disabled"]],
       ['*::after', []],
     ].forEach(([sel, result]) => {
-      expect(createSelectors(sel).map(s => (s.value || s.name))).toEqual(result)
+      const selector = createSelectors(sel).flat();
+      expect(selector.map(s => (s.value || s.name))).toEqual(result)
     })
   });
 });
