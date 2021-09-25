72c54ea7a6570bcea7ef3f088ed18e6c7b4b2d08
Elson Correia <design@elsoncorreia.com>
2021-09-25 02:15:50 -0400

    idem

--- core/parser/utils/regexPatterns.js
+++ core/parser/utils/regexPatterns.js
@@ -11,11 +11,5 @@ module.exports = {
   specificAttrPattern: (name) => new RegExp(`(#?${name})(?:\\s*=\\s*(?:"([^"]*)"|'([^']*)'|(\\S+)))?`, 'ig'),
   get selfClosingPattern() {
     return /^area|base|br|col|embed|hr|img|input|link|meta|param|source|track|wbr|doctype$/i
-  },
-  get selectors() {
-    return /(\+|\~|\>|\*)|:([a-z][a-z-]*)(?:\(([^)]*)\))?|\[\s*([a-z][\w:.-]*)\s*(?:\s*(\*|\||\^|\$|\~)?\s*=\s*"([^"]*)")?\s*(i|s)?\s*\]|(?:#|\.)?([a-z][\w-]*)|(?<=[^>+~\s])([ \t]+)(?=[^>+~\s])/gi
-  },
-  get pseudoClass() {
-    return /:(root|not|disabled|enabled|checked|blank|read-only|read-write|required|optional|empty|nth-last-child|nth-child|first-child|last-child|only-child|nth-of-type|nth-last-of-type|first-of-type|last-of-type|only-of-type)/
-  },
+  }
 }
